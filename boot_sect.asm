1c111e2d19ce102b752c621f4f90d49b7026a42c
Cromvell <nverzn@gmail.com>
2019-01-03 23:53:58 +0300

    Add disk read

--- boot_sect.asm
+++ boot_sect.asm
@@ -3,22 +3,34 @@
 ;
 [org 0x7c00]
 	
-	mov dx, 0x1fb6	; Pass hex value to print_hex function
-	call print_hex	; Print hex value
+	mov [BOOT_DRIVE], dl	; Save number of current boot drive
 
-	jmp $			; Halt
+	mov bp, 0x8000			; Initialize stack base and 
+	mov sp, bp				; stack pointer to 0x8000
 
-%include "funcs.asm"
+	mov bx, 0x9000			; Load 5 sectors to 0x0000(ES):0x9000(BX)
+	mov dh, 5				; from the boot disk.
+	mov dl, [BOOT_DRIVE]
+	call disk_load
 
-string:
-	db 'Hello, world!', 0
+	mov dx, [0x9000]		; Print first word 0xdada
+	call print_hex
 
-another_string:
-	db ' This is OS0', 0
+	mov dx, [0x9000 + 512]	; Print word from 2nd sector 0xface
+	call print_hex
 
-;
-;   Padding and magic BIOS number
-;
+	jmp $	; Halt
 
-	times 510-($-$$) db 0
-	dw 0xaa55
+%include "print_string.asm"
+%include "print_hex.asm"
+%include "disk_load.asm"
+
+; Global vars
+BOOT_DRIVE: db 0
+
+; Bootsector padding
+times 510-($-$$) db 0
+dw 0xaa55
+
+times 256 dw 0xdada
+times 256 dw 0xface
