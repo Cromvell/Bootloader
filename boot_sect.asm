7a65c78611148041cc40fcd85d13885820638b78
Cromvell <nverzn@gmail.com>
2019-01-04 13:17:05 +0300

    Switch to 32-bit protected mode accomplished

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,36 +1,35 @@
-;
-;   Hello, world!
-;
+
+;	A boot sector that enters 32-bit protected mode.
 [org 0x7c00]
 	
-	mov [BOOT_DRIVE], dl	; Save number of current boot drive
+	mov bp, 0x8000			; Set the stack. 
+	mov sp, bp
 
-	mov bp, 0x8000			; Initialize stack base and 
-	mov sp, bp				; stack pointer to 0x8000
+	mov bx, MSG_REAL_MODE
+	call print_string
 
-	mov bx, 0x9000			; Load 5 sectors to 0x0000(ES):0x9000(BX)
-	mov dh, 5				; from the boot disk.
-	mov dl, [BOOT_DRIVE]
-	call disk_load
+	call switch_to_pm		; We are never return from here
 
-	mov dx, [0x9000]		; Print first word 0xdada
-	call print_hex
-
-	mov dx, [0x9000 + 512]	; Print word from 2nd sector 0xface
-	call print_hex
-
-	jmp $	; Halt
+	jmp $
 
 %include "print_string.asm"
-%include "print_hex.asm"
-%include "disk_load.asm"
+%include "gdt.asm"
+%include "print_string_pm.asm"
+%include "switch_to_pm.asm"
 
-; Global vars
-BOOT_DRIVE: db 0
+[bits 32]
+; This is where we arrive after switching to and initializing protected mode.
+BEGIN_PM:
+
+	mov ebx, MSG_PROT_MODE
+	call print_string_pm	; Use 32-bit print routine.
+
+	jmp $					; Hang
+
+; Global variables
+MSG_REAL_MODE db "Started in 16-bit Real Mode", 0
+MSG_PROT_MODE db "Successfully landed in 32-bit Protected Mode!", 0
 
 ; Bootsector padding
 times 510-($-$$) db 0
 dw 0xaa55
-
-times 256 dw 0xdada
-times 256 dw 0xface
