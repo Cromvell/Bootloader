a970e7c7c1d4af8b46ed5151af613429990c73c4
Cromvell <nverzn@gmail.com>
2019-01-09 16:18:40 +0300

    Add screen driver

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,8 +1,6 @@
+#include "../drivers/screen.h"
 
 void main() {
-    unsigned char* vidmem = (unsigned char*)0xb8000;
-
-    vidmem[1] = 'X';
-    vidmem[3] = 'Y';
-    vidmem[5] = 'Z';
+	clear_screen();
+    print("Hello, motherfucking world!");
 }
---
+++ kernel/utils.h
@@ -0,0 +1,3 @@
+
+// Copy bytes from one place to another
+void memory_copy(char* source, char* dest, int no_bytes);
